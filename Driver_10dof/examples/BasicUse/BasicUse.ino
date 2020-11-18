3bca97eb5a1fa884dfff7d27843940da9532461f
Silmaen <genteur.slayer@laposte.net>
2020-11-18 16:19:09 +0100

    work on the 10dofs IMU accelerometer

--- Driver_10dof/examples/BasicUse/BasicUse.ino
+++ Driver_10dof/examples/BasicUse/BasicUse.ino
@@ -1,12 +1,37 @@
-#include <Arduino.h>
 #include <imu10dof.h>
+#include <Arduino.h>
+
+constexpr uint16_t bufsize = 1000;
 
 imu10dof myDevice;
 
-void setup() {
+struct accInfo {
+    uint64_t timestamp;
+    vec3f accel;
+};
 
+accInfo  buffer[bufsize];
+uint16_t iterator = 0;
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial.available());
+    Serial.println("System in ready.");
 }
 
 void loop() {
-
+    iterator = 0;
+    Serial.println("Measuring...");
+    uint64_t starttime = micros();
+    while(iterator<bufsize) {
+        buffer[iterator].timestamp = micros();
+        buffer[iterator].accel     = myDevice.measureAccel();
+        ++iterator;
+    }
+    uint64_t stroptime = micros();
+    Serial.print("End measure: ");
+    Serial.print((stroptime-starttime)/1e6);
+    Serial.println("s of measure.");
+    Serial.println("Waiting 10seconds.");
+    delay(10000); //
 }
