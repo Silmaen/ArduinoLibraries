9b9e9b4a0d817e4c32b736d97b6f239315a93a83
Silmaen <genteur.slayer@laposte.net>
2020-12-29 09:08:19 +0100

    vibration improvement

--- Driver_10dof/examples/Vibration/Vibration.ino
+++ Driver_10dof/examples/Vibration/Vibration.ino
@@ -3,34 +3,81 @@
 
 lsm303::Accel myDevice;
 
-static uint64_t    last_ts, ts;
-static math::vec3f last_acc, acc, vibe;
+static uint64_t    ts, tstart;
+static math::vec3f acc;
+uint8_t            mode = 0;
+String             message;
+uint64_t           measureDelay = 5000000;
 
 void setup() {
     Serial.begin(115200);
     while (!Serial)
         ;
-    Serial.println("System is starting.");
+    Serial.println(F("System is starting."));
     if (!myDevice.begin()) {
-        Serial.println("Problem during initializing");
+        Serial.println(F("Problem during initializing"));
         myDevice.print_config();
         while (1)
             ;
     }
     myDevice.setRate(lsm303::Accel::Rate::HZ_ULTRA);
-    Serial.println("System in ready.");
-    last_acc = myDevice.measure();
-    last_ts  = micros();
+    Serial.println(F("System in ready."));
+    Serial.println(F("RDY"));
 }
 
 void loop() {
-    acc = myDevice.measure();
-    ts  = micros();
-    Serial.print((int)ts);
-    Serial.print(" ");
-    Serial.print(acc.x(), 6);
-    Serial.print(" ");
-    Serial.print(acc.y(), 6);
-    Serial.print(" ");
-    Serial.println(acc.z(), 6);
+    if (mode == 0) {
+        if (Serial.available()) {
+            message = Serial.readStringUntil('\n');
+            Serial.println(F("ACK"));
+            if (message.startsWith(F("quiestu"))) {
+                Serial.println(F("DeviceVibration"));
+                Serial.println(F("RDY"));
+                return;
+            }
+            if (message.startsWith(F("measure"))) {
+                Serial.println(F("Begin Measure"));
+                tstart = micros();
+                mode   = 1;
+                Serial.println(F("MES"));
+                return;
+            }
+            if (message.startsWith(F("setmtime"))) {
+                int    a   = message.indexOf(' ');
+                String val = message.substring(a);
+                val.trim();
+                measureDelay = val.toInt();
+                Serial.print(F("Measure delay set to: "));
+                Serial.println((int)measureDelay);
+                Serial.println(F("RDY"));
+                return;
+            }
+            if (message.startsWith(F("getmtime"))) {
+                Serial.println((int)measureDelay);
+                Serial.println(F("RDY"));
+                return;
+            }
+            Serial.println(F("Unknown Command."));
+        }
+        ts = micros() - tstart;
+        if (ts > 1000000) {
+            tstart = micros();
+            Serial.println(F("RDY"));
+        }
+
+    } else {
+        acc = myDevice.measure();
+        ts  = micros() - tstart;
+        Serial.print((int)ts);
+        Serial.print(" ");
+        Serial.print(acc.x(), 6);
+        Serial.print(" ");
+        Serial.print(acc.y(), 6);
+        Serial.print(" ");
+        Serial.println(acc.z(), 6);
+        if (ts > measureDelay) {
+            mode = 0;
+            Serial.println(F("RDY"));
+        }
+    }
 }
